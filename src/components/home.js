a23dc51279e74c2a89d4cfb5e2a792c1336a6938
muzammilhussainshah <mynameismuzammilhussainshah@gmail.com>
2019-11-22 02:53:05 +0500

    working on home screen

--- src/components/home.js
+++ src/components/home.js
@@ -7,47 +7,102 @@ import { Navbar, Nav, Button, Form, FormControl, Row, Col, Container, Layout } f
 class Home extends Component {
     constructor(props) {
         super(props);
-        this.state = { height: props.height,width:props.width };
+        this.state = { height: props.height, width: props.width };
     }
 
     componentWillMount() {
-        this.setState({ height: window.innerHeight,width:window.innerWidth });
+        this.setState({ height: window.innerHeight, width: window.innerWidth });
     }
 
 
     render() {
         return (
-            <div style={{ backgroundColor: "#F0F0F0", height: this.state.height, }}>
-            <Row style={{ backgroundColor: "#F0F0F0",padding:"1%" }}  className="border-bottom border-1 ">
-                <Col xs={7}>
-                    <div style={{ marginLeft: "20%" }}>
-                        <img src={require('../assets/logo.png')} alt="aaaa" />
+            <div style={{ backgroundColor: "#fff", height: this.state.height, }}>
+                <Row style={{ backgroundColor: "#fff", padding: "1%" }} className="border-bottom border-1 ">
+                    <Col xs={7}>
+                        <div style={{ marginLeft: "20%" }}>
+                            <img src={require('../assets/logo2.png')} alt="aaaa" />
+                        </div>
+                    </Col>
+                    <Col xs={5}>
+
+                        <div style={{ marginTop: "1%" }} >
+
+                            <a href={"#"}>   <span style={{ marginLeft: "5%", fontWeight: "bold", color: "black" }}>Why choose us</span></a>
+                            <a href={"#"}>   <span style={{ marginLeft: "5%", fontWeight: "bold", color: "black" }}>How it works</span></a>
+                            <a href={"#"}>   <span style={{ marginLeft: "5%", fontWeight: "bold", color: "black" }}>Pricing</span></a>
+                            <a href={"#"}>   <span style={{ marginLeft: "5%", fontWeight: "bold", color: "black" }}>Profile</span></a>
+
+                            <span style={{ marginLeft: "5%", fontWeight: "bold" }}>
+
+                                {/* <Button variant="outline-primary">Signup</Button> */}
+
+                            </span>
+                        </div>
+                    </Col>
+                </Row>
+                <center>
+                    <div style={{ marginTop: "3%" }}>
+                        <a href={"#"}>   <span style={{ marginLeft: "5%", fontWeight: "bold", color: "black" }}>1. Account details</span></a>
+                        <a href={"#"}>   <span style={{ marginLeft: "5%", fontWeight: "bold", color: "black" }}>2. Business addres</span></a>
+                        <a href={"#"}>   <span style={{ marginLeft: "5%", fontWeight: "bold", color: "black" }}>3. Opening hours</span></a>
+                        <a href={"#"}>   <span style={{ marginLeft: "5%", fontWeight: "bold", color: "black" }}>4. Doctors</span></a>
+                        <a href={"#"}>   <span style={{ marginLeft: "5%", fontWeight: "bold", color: "black" }}>5. Doctors</span></a>
                     </div>
-                </Col>
-                <Col xs={5}>
+                </center>
+                {/* <center> */}
+
+                    <div style={{
+                        backgroundColor: "#fff", width: 360, padding: "1%", marginTop: "7%",
+                        borderRadius: 10,
+                        // webkitBoxShadow: "3px 3px 3px #9E9E9E",
+                        // mozBoxShadow: "3px 3px 3px #9E9E9E",
+                        // boxShadow: "3px 3px 3px #9E9E9E",
+                        height: 550,  position: "absolute",
+                        left: "40%",
+
+                    }} >
+
+                      
+                     
+
+                        <Form style={{ width: "100", marginTop: "7%" }}>
+                            <Form.Group >
+                            <Form.Label>Email address</Form.Label>
+                                <Form.Control type="text" placeholder="Name" />
+
+                            </Form.Group>
+                            <Form.Group controlId="formBasicEmail">
+                            <Form.Label>Email address</Form.Label>
+
+                                <Form.Control type="email" placeholder="Enter email" />
+
+                            </Form.Group>
+                            <Form.Group controlId="formBasicPassword">
+                            <Form.Label>Email address</Form.Label>
+
+                                <Form.Control type="password" placeholder="Enter password" />
+                            </Form.Group>
+
+                            <Form.Group controlId="formBasicPassword">
+                            <Form.Label>Email address</Form.Label>
+
+                                <Form.Control type="password" placeholder="Confirm password" />
+                            </Form.Group>
 
 
-                    <div style={{ marginTop: "1%" }} >
+                        </Form>
+                        
+                      
 
-                    <a href={"#"}>   <span style={{ marginLeft: "5%", fontWeight: "bold",color:"black" }}>Why choose us</span></a>
-                    <a href={"#"}>   <span style={{ marginLeft: "5%", fontWeight: "bold",color:"black" }}>How it works</span></a>
-                    <a href={"#"}>   <span style={{ marginLeft: "5%", fontWeight: "bold",color:"black" }}>Pricing</span></a>
-                    <a href={"#"}>   <span style={{ marginLeft: "5%", fontWeight: "bold",color:"black" }}>Login</span></a>
-                        {/* <span style={{ marginLeft: "5%", fontWeight: "bold" }}>How it works</span>
-                        <span style={{ marginLeft: "5%", fontWeight: "bold" }}>Pricing</span>
-                        <span style={{ marginLeft: "5%", fontWeight: "bold",color:"#2489FF" }}>Login</span> */}
-                        <span style={{ marginLeft: "5%", fontWeight: "bold" }}>
+                        <div style={{marginLeft:"80%"}}>
+                        <Button variant="outline-primary"  >Next</Button>
 
-                            <Button variant="outline-primary">Signup</Button>
+                        </div>
 
-
-                        </span>
                     </div>
 
-
-
-                </Col>
-            </Row>
+                {/* </center> */}
 
 
 
@@ -55,9 +110,7 @@ class Home extends Component {
 
 
 
-
-
-        </div>
+            </div>
         )
     }
 }
