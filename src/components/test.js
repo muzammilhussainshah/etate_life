280be9400818528b59e7c3dbcf1c5ab1af4ad6d2
muzammilhussainshah <mynameismuzammilhussainshah@gmail.com>
2019-11-24 19:11:54 +0500

    working on pricing footer

--- src/components/test.js
+++ src/components/test.js
@@ -1,10 +1,11 @@
 import React, { Component, } from 'react';
 // import { Layout, Menu, Breadcrumb,Row, Col, } from 'antd';
-import { Navbar, Nav, Button, Form, FormControl, Row, Col, Container, Layout, NavDropdown, Card } from 'react-bootstrap';
+import { Navbar, Nav, Button, Form, FormControl, Row, Col, Container, Layout, NavDropdown, Card, Jumbotron } from 'react-bootstrap';
 import AppHeader from './common/AppHeader';
 // import {} from 'bootstrap';
 // import { ButtonToolbar,DropdownButton,Dropdown , Navbar,Nav,NavDropdown} from 'react-bootstrap';
 import styles from './style.css';
+import { FaLevelUpAlt,FaAngleDoubleRight } from 'react-icons/fa';
 
 // const { Header, Footer, Sider, Content } = Layout;
 
@@ -25,7 +26,7 @@ class Test extends Component {
   }
   render() {
     return (
-      <div style={{ backgroundColor: "#fff",}}>
+      <div style={{ backgroundColor: "#fff", }}>
         <AppHeader />
         {/* <div style={{ background: "#F0F3F1", }}>
           <hr />
@@ -53,7 +54,7 @@ class Test extends Component {
                 <Card.Text style={{ marginTop: "5%" }}>
                   1
             </Card.Text>
-                <Button style={{ background: "#3C6AB3", borderColor: "#3C6AB3", marginTop: "10%" }} variant="primary">Go somewhere</Button>
+                <Button style={{ background: "#3C6AB3", borderColor: "#3C6AB3", marginTop: "10%" }} variant="primary">Buy Now</Button>
 
               </Card.Body>
             </Card><Card style={{ width: '18rem', paddingTop: "2%", paddingBottom: "2%" }}>
@@ -69,7 +70,7 @@ class Test extends Component {
                 <Card.Text style={{ marginTop: "5%" }}>
                   1
             </Card.Text>
-                <Button style={{ background: "#3C6AB3", borderColor: "#3C6AB3", marginTop: "10%" }} variant="primary">Go somewhere</Button>
+                <Button style={{ background: "#3C6AB3", borderColor: "#3C6AB3", marginTop: "10%" }} variant="primary">Buy Now</Button>
 
               </Card.Body>
             </Card><Card style={{ width: '18rem', paddingTop: "2%", paddingBottom: "2%" }}>
@@ -85,7 +86,7 @@ class Test extends Component {
                 <Card.Text style={{ marginTop: "5%" }}>
                   1
             </Card.Text>
-                <Button style={{ background: "#3C6AB3", borderColor: "#3C6AB3", marginTop: "10%" }} variant="primary">Go somewhere</Button>
+                <Button style={{ background: "#3C6AB3", borderColor: "#3C6AB3", marginTop: "10%" }} variant="primary">Buy Now</Button>
 
               </Card.Body>
             </Card><Card style={{ width: '18rem', paddingTop: "2%", paddingBottom: "2%" }}>
@@ -101,43 +102,140 @@ class Test extends Component {
                 <Card.Text style={{ marginTop: "5%" }}>
                   1
             </Card.Text>
-                <Button style={{ background: "#3C6AB3", borderColor: "#3C6AB3", marginTop: "10%" }} variant="primary">Go somewhere</Button>
+                <Button style={{ background: "#3C6AB3", borderColor: "#3C6AB3", marginTop: "10%" }} variant="primary">Buy Now</Button>
 
               </Card.Body>
             </Card>
           </div>
         </center>
         {/* switch */}
-        <div style={{marginTop:"5%",display:"flex",justifyContent:"center"}}>
-            <div style={{marginRight:"1%",fontWeight:"bold"}} >
-            Month 
+        <div style={{ marginLeft: "12%", color: "#3C6AB3", marginTop: "2%", fontSize: 13, display: "flex", justifyContent: "center" }} >
+          You save 17 %
         </div>
-        <div className='custom-control custom-switch'>
-        {/* <span >
+        <div style={{ marginTop: "1%", display: "flex", justifyContent: "center" }}>
+          <div style={{ marginRight: "1%", fontWeight: "bold" }} >
+            Month
+        </div>
+          <div className='custom-control custom-switch'>
+            {/* <span >
             Year
         </span> */}
-          <input
-            type='checkbox'
-            className='custom-control-input'
-            id='customSwitches'
-            checked={this.state.switch1}
-            onChange={this.handleSwitchChange(1)}
-            readOnly
-          />
-          <label className='custom-control-label' htmlFor='customSwitches'>
-            Year
-        </label>
-        </div>
+            <input
+              type='checkbox'
+              className='custom-control-input'
+              id='customSwitches'
+              checked={this.state.switch1}
+              onChange={this.handleSwitchChange(1)}
+              readOnly
+            />
+            <label className='custom-control-label' htmlFor='customSwitches'>
+              Year <FaLevelUpAlt style={{ color: "#3C6AB3", fontSize: 22 }} />
+            </label>
+          </div>
 
         </div>
         <center >
-          
-        <Button style={{ background: "#3C6AB3", borderColor: "#3C6AB3", marginTop: "3%" }} variant="primary" size="lg">Start free trial now!</Button>
-      <div style={{color:"grey",fontSize:11,marginTop:"1%"}}>
-        No credit card needed!
+
+          <Button style={{ background: "#3C6AB3", borderColor: "#3C6AB3", marginTop: "3%" }} variant="primary" size="lg">Start free trial now!</Button>
+          <div style={{ color: "grey", fontSize: 11, marginTop: "1%" }}>
+            No credit card needed!
       </div>
         </center>
 
+        <div style={{ background: "#EEF5FF", padding: "3%" }}>
+          <div style={{ display: "flex", flexBasis: "100%", justifyContent: "center", }}>
+            <div style={{ display: "flex", alignItems: "center" }}>
+              <div>
+
+                Do you need an <span style={{ color: "#3C6AB3" }}>Enterprise solution?</span>
+              </div>
+            </div>
+            <div style={{ marginLeft: "3%" }}>
+              <Button style={{ borderColor: "#3C6AB3", }} variant="outline-primary" size="lg">Schedule a Meeting</Button>
+            </div>
+          </div>
+        </div>
+        <div style={{ display: "flex", flexBasis: "100%", justifyContent: "center", background: "#1B1B1B" }}>
+          <div style={{ flexBasis: "20%", background: "red",padding:"2%" }}>
+            <h5 style={{color:"#fff"}}>
+              About us
+            </h5>
+            <div style={{color:"#fff"}}>
+             lorem ipsum  lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum 
+            </div>
+          </div>
+          <div style={{ flexBasis: "20%", background: "green",padding:"2%"  }}>
+          <h5 style={{color:"#fff"}}>
+              Site Map
+            </h5>
+            <div style={{color:"#fff"}}>
+             <FaAngleDoubleRight style={{  fontSize: 22 }} /> About us
+              </div>
+            <div style={{color:"#fff"}}>
+             <FaAngleDoubleRight style={{  fontSize: 22 }} /> Services
+              </div>
+            <div style={{color:"#fff"}}>
+             <FaAngleDoubleRight style={{  fontSize: 22 }} /> Testimonials
+              </div>
+            <div style={{color:"#fff"}}>
+             <FaAngleDoubleRight style={{  fontSize: 22 }} /> Gallery
+              </div>
+            <div style={{color:"#fff"}}>
+             <FaAngleDoubleRight style={{  fontSize: 22 }} /> Blog
+              </div>
+          </div>  
+          <div style={{ flexBasis: "20%", background: "brown" ,padding:"2%" }} >
+          <h5 style={{color:"#fff"}}>
+              Site Map
+            </h5>
+            <div style={{color:"#fff"}}>
+             <FaAngleDoubleRight style={{  fontSize: 22 }} /> About us
+              </div>
+            <div style={{color:"#fff"}}>
+             <FaAngleDoubleRight style={{  fontSize: 22 }} /> Services
+              </div>
+            <div style={{color:"#fff"}}>
+             <FaAngleDoubleRight style={{  fontSize: 22 }} /> Testimonials
+              </div>
+            <div style={{color:"#fff"}}>
+             <FaAngleDoubleRight style={{  fontSize: 22 }} /> Gallery
+              </div>
+            <div style={{color:"#fff"}}>
+             <FaAngleDoubleRight style={{  fontSize: 22 }} /> Blog
+              </div>
+          </div>
+          <div style={{ flexBasis: "20%", background: "brown" ,padding:"2%" }} >
+          <h5 style={{color:"#fff"}}>
+              FOLLOW US
+            </h5>
+            <div style={{color:"#fff"}}>
+            <a class="btn-floating btn-lg btn-fb" type="button" role="button"><i class="fab fa-facebook-f"></i></a>
+              </div>
+            <div style={{color:"#fff"}}>
+             <FaAngleDoubleRight style={{  fontSize: 22 }} /> Services
+              </div>
+            <div style={{color:"#fff"}}>
+             <FaAngleDoubleRight style={{  fontSize: 22 }} /> Testimonials
+              </div>
+            <div style={{color:"#fff"}}>
+             <FaAngleDoubleRight style={{  fontSize: 22 }} /> Gallery
+              </div>
+            <div style={{color:"#fff"}}>
+             <FaAngleDoubleRight style={{  fontSize: 22 }} /> Blog
+              </div>
+          </div>
+        </div>
+        {/* <div style={{ background: "#1B1B1B", }}>
+          <div style={{ display: "flex", flexBasis: "100%", justifyContent: "center", }}>
+            <div style={{ display: "flex", alignItems: "center" }}>
+              <div>
+                Do you need an <span style={{ color: "#3C6AB3" }}>Enterprise solution?</span></div>
+            </div>
+            <div style={{ marginLeft: "3%" }}>
+            ={{ borderColor: "#3C6AB3", }} variant="outline-primary" size="lg">Schedule a Meeting</Button>
+            </div>
+          </div>
+        </div> */}
 
       </div >
     )
