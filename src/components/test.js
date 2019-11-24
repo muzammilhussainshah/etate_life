3de3651d9732acc250cccea481c677399c10b607
muzammilhussainshah <mynameismuzammilhussainshah@gmail.com>
2019-11-24 17:38:37 +0500

    working on pricing body

--- src/components/test.js
+++ src/components/test.js
@@ -1,6 +1,6 @@
 import React, { Component, } from 'react';
 // import { Layout, Menu, Breadcrumb,Row, Col, } from 'antd';
-import { Navbar, Nav, Button, Form, FormControl, Row, Col, Container, Layout, NavDropdown ,Card} from 'react-bootstrap';
+import { Navbar, Nav, Button, Form, FormControl, Row, Col, Container, Layout, NavDropdown, Card } from 'react-bootstrap';
 import AppHeader from './common/AppHeader';
 // import {} from 'bootstrap';
 // import { ButtonToolbar,DropdownButton,Dropdown , Navbar,Nav,NavDropdown} from 'react-bootstrap';
@@ -11,104 +11,132 @@ import styles from './style.css';
 class Test extends Component {
   constructor(props) {
     super(props);
-    this.state = { height: props.height, width: props.width };
+    this.state = {
+      switch1: true,
+    };
   }
 
-  componentWillMount() {
-    this.setState({ height: window.innerHeight, width: window.innerWidth });
+
+  handleSwitchChange = nr => () => {
+    let switchNumber = `switch${nr}`;
+    this.setState({
+      [switchNumber]: !this.state[switchNumber]
+    });
   }
-
-
   render() {
     return (
-      <div style={{}}>
-
-       <AppHeader/>
+      <div style={{ backgroundColor: "#fff",}}>
+        <AppHeader />
         {/* <div style={{ background: "#F0F3F1", }}>
           <hr />
         </div> */}
-
         <div>
           <div style={{}}>
             <img style={{ width: "100%", }} src={require('../assets/image1.jpg')} alt="aaaa" />
           </div>
         </div>
-
-        <center style={{fontSize:25,fontWeight:"bold",marginTop:"3%"}}>
-        Pricing
+        <center style={{ fontSize: 25, fontWeight: "bold", marginTop: "3%" }}>
+          Pricing
         </center>
-<center>
+        <center>
+          <div style={{ display: "flex", flexBasis: "100%", justifyContent: "center", marginTop: "3%", alignItems: "center", }}>
+            <Card style={{ width: '18rem', paddingTop: "2%", paddingBottom: "2%" }}>
+              <Card.Body>
+                <Card.Title style={{ color: "blue", fontSize: 25 }}>Solo Clinic</Card.Title>
+                <Card.Title style={{ color: "blue", fontSize: 17 }}>$ 75 / month</Card.Title>
+                <Card.Text style={{ marginTop: "10%" }}>
+                  60 days
+            </Card.Text>
+                <Card.Text style={{ marginTop: "5%" }}>
+                  60 days
+            </Card.Text>
+                <Card.Text style={{ marginTop: "5%" }}>
+                  1
+            </Card.Text>
+                <Button style={{ background: "#3C6AB3", borderColor: "#3C6AB3", marginTop: "10%" }} variant="primary">Go somewhere</Button>
 
-<div style={{display:"flex",flexBasis:"100%",justifyContent:"center",marginTop:"3%",alignItems:"center",}}>
-<Card style={{ width: '18rem',paddingTop:"2%",paddingBottom:"2%" }}>
-  <Card.Body>
-    <Card.Title style={{color:"blue",fontSize:25}}>Solo Clinic</Card.Title>
-    <Card.Title style={{color:"blue",fontSize:17}}>$ 75 / month</Card.Title>
-    <Card.Text style={{marginTop:"10%"}}>
-    60 days
-    </Card.Text>
-    <Card.Text style={{marginTop:"5%"}}>
-    60 days
-    </Card.Text>
-    <Card.Text style={{marginTop:"5%"}}>
-    1
-    </Card.Text>
-    <Button style={{background:"purple",borderColor:"purple",marginTop:"10%"}} variant="primary">Go somewhere</Button>
+              </Card.Body>
+            </Card><Card style={{ width: '18rem', paddingTop: "2%", paddingBottom: "2%" }}>
+              <Card.Body>
+                <Card.Title style={{ color: "blue", fontSize: 25 }}>Solo Clinic</Card.Title>
+                <Card.Title style={{ color: "blue", fontSize: 17 }}>$ 75 / month</Card.Title>
+                <Card.Text style={{ marginTop: "10%" }}>
+                  60 days
+            </Card.Text>
+                <Card.Text style={{ marginTop: "5%" }}>
+                  60 days
+            </Card.Text>
+                <Card.Text style={{ marginTop: "5%" }}>
+                  1
+            </Card.Text>
+                <Button style={{ background: "#3C6AB3", borderColor: "#3C6AB3", marginTop: "10%" }} variant="primary">Go somewhere</Button>
 
-  </Card.Body>
-</Card><Card style={{ width: '18rem',paddingTop:"2%",paddingBottom:"2%" }}>
-  <Card.Body>
-    <Card.Title style={{color:"blue",fontSize:25}}>Solo Clinic</Card.Title>
-    <Card.Title style={{color:"blue",fontSize:17}}>$ 75 / month</Card.Title>
-    <Card.Text style={{marginTop:"10%"}}>
-    60 days
-    </Card.Text>
-    <Card.Text style={{marginTop:"5%"}}>
-    60 days
-    </Card.Text>
-    <Card.Text style={{marginTop:"5%"}}>
-    1
-    </Card.Text>
-    <Button style={{background:"purple",borderColor:"purple",marginTop:"10%"}} variant="primary">Go somewhere</Button>
+              </Card.Body>
+            </Card><Card style={{ width: '18rem', paddingTop: "2%", paddingBottom: "2%" }}>
+              <Card.Body>
+                <Card.Title style={{ color: "blue", fontSize: 25 }}>Solo Clinic</Card.Title>
+                <Card.Title style={{ color: "blue", fontSize: 17 }}>$ 75 / month</Card.Title>
+                <Card.Text style={{ marginTop: "10%" }}>
+                  60 days
+            </Card.Text>
+                <Card.Text style={{ marginTop: "5%" }}>
+                  60 days
+            </Card.Text>
+                <Card.Text style={{ marginTop: "5%" }}>
+                  1
+            </Card.Text>
+                <Button style={{ background: "#3C6AB3", borderColor: "#3C6AB3", marginTop: "10%" }} variant="primary">Go somewhere</Button>
 
-  </Card.Body>
-</Card><Card style={{ width: '18rem',paddingTop:"2%",paddingBottom:"2%" }}>
-  <Card.Body>
-    <Card.Title style={{color:"blue",fontSize:25}}>Solo Clinic</Card.Title>
-    <Card.Title style={{color:"blue",fontSize:17}}>$ 75 / month</Card.Title>
-    <Card.Text style={{marginTop:"10%"}}>
-    60 days
-    </Card.Text>
-    <Card.Text style={{marginTop:"5%"}}>
-    60 days
-    </Card.Text>
-    <Card.Text style={{marginTop:"5%"}}>
-    1
-    </Card.Text>
-    <Button style={{background:"purple",borderColor:"purple",marginTop:"10%"}} variant="primary">Go somewhere</Button>
+              </Card.Body>
+            </Card><Card style={{ width: '18rem', paddingTop: "2%", paddingBottom: "2%" }}>
+              <Card.Body>
+                <Card.Title style={{ color: "blue", fontSize: 25 }}>Solo Clinic</Card.Title>
+                <Card.Title style={{ color: "blue", fontSize: 17 }}>$ 75 / month</Card.Title>
+                <Card.Text style={{ marginTop: "10%" }}>
+                  60 days
+            </Card.Text>
+                <Card.Text style={{ marginTop: "5%" }}>
+                  60 days
+            </Card.Text>
+                <Card.Text style={{ marginTop: "5%" }}>
+                  1
+            </Card.Text>
+                <Button style={{ background: "#3C6AB3", borderColor: "#3C6AB3", marginTop: "10%" }} variant="primary">Go somewhere</Button>
 
-  </Card.Body>
-</Card><Card style={{ width: '18rem',paddingTop:"2%",paddingBottom:"2%" }}>
-  <Card.Body>
-    <Card.Title style={{color:"blue",fontSize:25}}>Solo Clinic</Card.Title>
-    <Card.Title style={{color:"blue",fontSize:17}}>$ 75 / month</Card.Title>
-    <Card.Text style={{marginTop:"10%"}}>
-    60 days
-    </Card.Text>
-    <Card.Text style={{marginTop:"5%"}}>
-    60 days
-    </Card.Text>
-    <Card.Text style={{marginTop:"5%"}}>
-    1
-    </Card.Text>
-    <Button style={{background:"purple",borderColor:"purple",marginTop:"10%"}} variant="primary">Go somewhere</Button>
+              </Card.Body>
+            </Card>
+          </div>
+        </center>
+        {/* switch */}
+        <div style={{marginTop:"5%",display:"flex",justifyContent:"center"}}>
+            <div style={{marginRight:"1%",fontWeight:"bold"}} >
+            Month 
+        </div>
+        <div className='custom-control custom-switch'>
+        {/* <span >
+            Year
+        </span> */}
+          <input
+            type='checkbox'
+            className='custom-control-input'
+            id='customSwitches'
+            checked={this.state.switch1}
+            onChange={this.handleSwitchChange(1)}
+            readOnly
+          />
+          <label className='custom-control-label' htmlFor='customSwitches'>
+            Year
+        </label>
+        </div>
 
-  </Card.Body>
-</Card>
-</div>
-</center>
-
-
+        </div>
+        <center >
+          
+        <Button style={{ background: "#3C6AB3", borderColor: "#3C6AB3", marginTop: "3%" }} variant="primary" size="lg">Start free trial now!</Button>
+      <div style={{color:"grey",fontSize:11,marginTop:"1%"}}>
+        No credit card needed!
+      </div>
+        </center>
 
 
       </div >
